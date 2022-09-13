df541840e48eb6b574ca7c5dc70abf3417e68705
Ji Seungmin <cswcsm02@gmail.com>
2022-09-13 20:55:15 +0900

    ViewModel / LiveData / LifeCycleOwner

--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.jetpackstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.jetpackstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.vm = viewModel //vm은 xml에 만들어 둔 것
+        binding.lifecycleOwner = this
+
+        binding.next.setOnClickListener {
+            viewModel.getNextData()
+        }
     }
 }

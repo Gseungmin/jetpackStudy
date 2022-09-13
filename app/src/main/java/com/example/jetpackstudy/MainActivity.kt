ed55d208ae597fee231e2dc78047c4e2fc15ecb5
Ji Seungmin <cswcsm02@gmail.com>
2022-09-13 15:36:45 +0900

    dataBinding 동적 할당

--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.jetpackstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.example.jetpackstudy.databinding.ActivityMainBinding
 
+//DataBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var testCount = 20
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //기존 방법
+        //binding.text.text = "바뀐 텍스트"
+
+        //데이터 결합 방법
+        val person = Person("ji", 20)
+        binding.person = person
+    }
+
+    fun btnClick(view: View) {
+        testCount++
+
+        val person = Person("ji", testCount)
+        binding.person = person
     }
 }

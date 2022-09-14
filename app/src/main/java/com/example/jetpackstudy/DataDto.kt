6c6cab0a40351d62a958c35ebd5fb597a0e2263d
Ji Seungmin <cswcsm02@gmail.com>
2022-09-14 15:35:19 +0900

    Retrofit 기본 사용 - GetMapping, Dto 기본 설정 및 사용 및 테스트

---
+++ app/src/main/java/com/example/jetpackstudy/DataDto.kt
@@ -0,0 +1,10 @@
+package com.example.jetpackstudy
+
+//dto 역할
+data class DataDto (
+
+    val userId : Int,
+    val id : Int,
+    val title : String,
+    val body : String
+)
--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.jetpackstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
+// Simple Retrofit Ex
+// https://jsonplaceholder.typicode.com/
+// https://jsonplaceholder.typicode.com/posts
+// https://jsonplaceholder.typicode.com/posts/3
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val api = RetrofitInstance.getInstance().create(MyApi::class.java)
+        api.getPost1().enqueue(object : Callback<DataDto>{
+            //응답 성공시
+            override fun onResponse(call: Call<DataDto>, response: Response<DataDto>) {
+                Log.d("API1", response.body().toString())
+            }
+            //응답 실패시
+            override fun onFailure(call: Call<DataDto>, t: Throwable) {
+                Log.d("API1", "fail")
+            }
+        })
+
+        api.getPosts().enqueue(object : Callback<MutableList<DataDto>>{
+            override fun onResponse(
+                call: Call<MutableList<DataDto>>,
+                response: Response<MutableList<DataDto>>
+            ) {
+                Log.d("API2", response.body().toString())
+            }
+            override fun onFailure(call: Call<MutableList<DataDto>>, t: Throwable) {
+                Log.d("API2", "fail")
+            }
+        })
+
+        api.getPostNum(5).enqueue(object : Callback<DataDto>{
+            //응답 성공시
+            override fun onResponse(call: Call<DataDto>, response: Response<DataDto>) {
+                Log.d("API3", response.body().toString())
+            }
+            //응답 실패시
+            override fun onFailure(call: Call<DataDto>, t: Throwable) {
+                Log.d("API3", "fail")
+            }
+        })
     }
 }

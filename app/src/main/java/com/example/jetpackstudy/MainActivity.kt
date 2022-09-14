f6bdee07cd8b2120e2b36eee83711b23cb8d79d4
Ji Seungmin <cswcsm02@gmail.com>
2022-09-14 23:58:37 +0900

    WorkManager 주기적 실행

--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.jetpackstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
 
+// 주기적 실행
+// 15분마다 / 30분다 / 1시간마다 로직 실행
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //15분마다 실행, 미니멈이 15분
+        val workManager = PeriodicWorkRequestBuilder<WorkManager1>(15, TimeUnit.MINUTES).build()
+        WorkManager.getInstance(this).enqueue(workManager)
     }
 }

38ff3c63620f4fa1865d435f0483d834d1385406
Ji Seungmin <cswcsm02@gmail.com>
2022-09-14 17:52:35 +0900

    insert + delete + UI 추가

--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.jetpackstudy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,9 +17,29 @@ class MainActivity : AppCompatActivity() {
 
         val db = Database.getDatabase(this)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            db.textDao().insert(Entity(0, "HELLO"))
-            Log.d("MAINACTIVITY", db.textDao().getAllData().toString())
+        val inputArea = findViewById<EditText>(R.id.textInputArea)
+        val insertBtn = findViewById<Button>(R.id.insert)
+        val getAllBtn = findViewById<Button>(R.id.getData)
+        val deleteBtn = findViewById<Button>(R.id.delete)
+
+        insertBtn.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                db.textDao().insert(Entity(0, inputArea.text.toString()))
+                inputArea.setText("")
+            }
+        }
+
+        getAllBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.d("MAINACTIVITY", db.textDao().getAllData().toString())
+            }
+        }
+
+        deleteBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.textDao().deleteAll()
+            }
         }
     }
 }

27de833bab69fb1522ebe92cb1bc04716262cfb5
Ji Seungmin <cswcsm02@gmail.com>
2022-09-14 23:18:11 +0900

    workManager Chaining

--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.jetpackstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 
+//workManager Chaining : 순서 설정
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val workManagerA = OneTimeWorkRequestBuilder<WorkManagerA>().build()
+        val workManagerB = OneTimeWorkRequestBuilder<WorkManagerB>().build()
+        val workManagerC = OneTimeWorkRequestBuilder<WorkManagerC>().build()
+
+        //기존 방식
+//        WorkManager.getInstance(this).enqueue(workManagerA)
+//        WorkManager.getInstance(this).enqueue(workManagerB)
+//        WorkManager.getInstance(this).enqueue(workManagerC)
+
+        //A와 B가 끝난 후 C 시작
+        WorkManager.getInstance(this)
+            .beginWith(listOf(workManagerA, workManagerB))
+            .then(workManagerC)
+            .enqueue()
     }
 }
